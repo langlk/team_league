712f2ab0987ebc155205cd57756c68dabb5de847
Dan Nollette and Kelsey Langlois <dan@>
2017-09-25 10:59:33 -0700

    Add UI for adding and listing teams.

--- app.rb
+++ app.rb
@@ -6,3 +6,13 @@ require('./lib/player')
 require('./lib/team')
 require('pg')
 require "pry"
+
+get('/') do
+  @teams = Team.all
+  erb(:index)
+end
+
+post('/') do
+  Team.create({name: params["name"]})
+  redirect '/'
+end
